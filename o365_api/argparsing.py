67106d8f74c8a1bac727579ba8af06d26188c435
krayzpipes <35900438+krayzpipes@users.noreply.github.com>
2018-08-10 14:34:46 -0400

    Create argparsing.py

---
+++ o365_api/argparsing.py
@@ -0,0 +1,17 @@
+
+def add_arg_parser_args(argument_parser):
+    argument_parser.add_argument(
+        "-s",
+        "--start_time",
+        help="Integer representing the desired start time in epoch format",
+    )
+    argument_parser.add_argument(
+        "-e",
+        "--end_time",
+        help="Integer representing the desired stop time in epoch format",
+    )
+    argument_parser.add_argument(
+        "-l",
+        "--log_level",
+        help="Set logging level",
+    )
